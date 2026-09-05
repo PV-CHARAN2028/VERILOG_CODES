0c1da52e500d0fc5999544cb41403621ecd232a0
CHARAN KUMAR PEDDIBOINA <charanyadavpeddiboina@gmail.com>
2026-09-05 13:43:14 +0530

    Remove copyright notice and clean up formatting

--- alu.v
+++ alu.v
@@ -27,22 +27,22 @@ module alu(input [7:0]a_in,b_in,
 	   output [15:0]d_out);
 
    parameter 	
-                 ADD  = 4'b0000, // Add two 8 bit numbers a and b.
-		 INC  = 4'b0001, // Increment a by 1. 
-		 SUB  = 4'b0010, // Subtracts b from a.
-		 DEC  = 4'b0011, // Decrement a by 1.
-		 MUL  = 4'b0100, // Multiply 4 bit numbers a and b.
-		 DIV  = 4'b0101, // Divide a by b.
-		 SHL  = 4'b0110, // Shift a to left side by 1 bit.
-		 SHR  = 4'b0111, // Shift a to right by 1 bit.
-		 AND  = 4'b1000, // Logical AND operation
-	         OR   = 4'b1001, // Logical OR operation
-		 INV  = 4'b1010, // Logical Negation
-		 NAND = 4'b1011, // Bitwise NAND
-		 NOR  = 4'b1100, // Bitwise NOR
-		 XOR  = 4'b1101, // Bitwise XOR
-		 XNOR = 4'b1110, // Bitwise XNOR
-		 BUF  = 4'b1111; // BUF
+        ADD  = 4'b0000,            // Add two 8 bit numbers a and b.
+		 INC  = 4'b0001,           // Increment a by 1. 
+		 SUB  = 4'b0010,           // Subtracts b from a.
+		 DEC  = 4'b0011,           // Decrement a by 1.
+		 MUL  = 4'b0100,           // Multiply 4 bit numbers a and b.
+		 DIV  = 4'b0101,           // Divide a by b.
+		 SHL  = 4'b0110,           // Shift a to left side by 1 bit.
+		 SHR  = 4'b0111,           // Shift a to right by 1 bit.
+		 AND  = 4'b1000,           // Logical AND operation
+	         OR   = 4'b1001,       // Logical OR operation
+		 INV  = 4'b1010,           // Logical Negation
+		 NAND = 4'b1011,           // Bitwise NAND
+		 NOR  = 4'b1100,           // Bitwise NOR
+		 XOR  = 4'b1101,           // Bitwise XOR
+		 XNOR = 4'b1110,           // Bitwise XNOR
+		 BUF  = 4'b1111;           // BUF
 
    //Internal variable used during ALU operation
    reg  [15:0]out;
@@ -55,22 +55,22 @@ module alu(input [7:0]a_in,b_in,
 	 case(command_in)
             //--------- write the functionality here -------  
                
-                 ADD  :out =a_in+b_in; // Add two 8 bit numbers a and b.
-		 INC  :out =a_in+1'b1;// Increment a by 1. 
-		 SUB  :out =a_in-b_in;  // Subtracts b from a.
-		 DEC  :out =a_in-1'b1;// Decrement a by 1.
-		 MUL  :out =a_in*b_in;// Multiply 4 bit numbers a and b.
-		 DIV  :out =a_in/b_in;// Divide a by b.
-		 SHL  :out = a_in<<1;
-		 SHR  :out =a_in>>1;
-		 AND  :out =a_in&b_in;
-	         OR   :out =a_in|b_in;
-		 INV  :out =!a_in;
-		 NAND :out =~(a_in&b_in);
-		 NOR  :out =~(a_in|b_in);
-		 XOR  :out = a_in^b_in;
-		 XNOR :out =~(a_in^b_in);
-		 BUF  :out  = a_in;
+         ADD  :out =a_in+b_in;                    // Add two 8 bit numbers a and b.
+		 INC  :out =a_in+1'b1;                    // Increment a by 1. 
+		 SUB  :out =a_in-b_in;                    // Subtracts b from a.
+		 DEC  :out =a_in-1'b1;                    // Decrement a by 1.
+		 MUL  :out =a_in*b_in;                    // Multiply 4 bit numbers a and b.
+		 DIV  :out =a_in/b_in;                    // Divide a by b.
+		 SHL  :out = a_in<<1;                     //shift a to left by 1'b1. 
+		 SHR  :out =a_in>>1;                      //shift a to right by 1'b1.
+		 AND  :out =a_in&b_in;                    //logical and(&) operation.
+		 OR   :out =a_in|b_in;                    //logical or(|) operaion.
+		 INV  :out =!a_in;                        //bitwise inverse operation.
+		 NAND :out =~(a_in&b_in);                 //bitwise nand operation .
+		 NOR  :out =~(a_in|b_in);                 //bitwise nor operation .
+		 XOR  :out = a_in^b_in;                   //bitwise xor operation.
+		 XNOR :out =~(a_in^b_in);                 //bitwise xnor oeration.
+		 BUF  :out  = a_in;                       //buffer.
 	 endcase
       end
 
